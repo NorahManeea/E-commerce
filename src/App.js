fa8570727807598d5bdb2a347e427bffa609a7e8
Norah Almaneea <nouraalmanea0@gmail.com>
2023-05-25 14:58:15 +0300

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import Header from './Header'
-import Footer from './Footer'
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Container><h1>Test</h1></Container>
       <Footer/>
     </div>
   );
