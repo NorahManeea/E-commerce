d94a9840569355284196f7eb3eab550f410b1927
Norah Almaneea <nouraalmanea0@gmail.com>
2023-05-28 02:06:36 +0300

    Listing Products names

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import {Container} from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <main className="py-3">
-      <Container><h1>Test</h1></Container>
+      <Container>
+      <HomeScreen/>
+        </Container>
       </main>
+     
       <Footer/>
     </div>
   );
