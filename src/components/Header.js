70d012c4036757bc7b0c9c11f99d4c8655a1b9c2
Norah Almaneea <nouraalmanea0@gmail.com>
2023-05-29 02:28:47 +0300

    building Products details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
   return (
     <header>
-     <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
 
-            <Navbar.Brand>E-COMMERCE APP</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                  <i className='fas fa-shopping-cart'></i> Cart
-                  </Nav.Link>
-                <Nav.Link href='/login'>
-                    <i className='fas fa-user'></i> Sign In
-                  </Nav.Link>
-                  </Nav>
-                  </Navbar.Collapse>
+              <LinkContainer to="/signin">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-        </Navbar>
-  </header>
-  )
+      </Navbar>
+    </header>
+  );
 }
